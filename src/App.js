27b8e8a7edd873d8a85d5168325857e69653849e
Prashandip <prashandip2@gmail.com>
2022-05-17 23:56:05 +0545

    Fixed some issues

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import { useDispatch, useSelector } from "react-redux";
 import "./scss/main.scss";
+import { actions } from "./store";
 
 function App() {
   const counter = useSelector((state) => state.counter);
   const dispatch = useDispatch();
   const increment = () => {
-    dispatch({ type: "INC" });
+    dispatch(actions.increment());
   };
   const decrement = () => {
-    dispatch({ type: "DEC" });
+    dispatch(actions.decrement());
   };
-  const addBy = () => {
-    dispatch({ type: "ADD", payload: 10 });
+  const add10 = () => {
+    dispatch(actions.addBy(10));
+  };
+  const deduct10 = () => {
+    dispatch(actions.addBy(-10));
   };
   return (
     <section className="counter-app">
       <h1>Counter App</h1>
       <h2>{counter}</h2>
       <div className="buttons">
-        <button onClick={increment}>Increment</button>
+        <button onClick={deduct10}>Deduct 10</button>
         <button onClick={decrement}>Decrement</button>
-        <button onClick={addBy}>Add 10</button>
+        <button onClick={increment}>Increment</button>
+        <button onClick={add10}>Add 10</button>
       </div>
     </section>
   );
